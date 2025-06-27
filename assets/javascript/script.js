c69d8f3221e7075d7c0ff8c17c92757b56c0a165
Freddy León <90221188+Fredleo3@users.noreply.github.com>
2025-06-27 03:12:55 -0500

    Se agrega contenido, se estructura y se estila
    Se agregan las secciones educación y proyectos.
    
    Se ajustan los estilos de la sección habilidades.
    
    Se estilan las nuevas secciones.
    
    Se agregan imágenes a la carpeta img.
    
    Se agregan animaciones a las imágenes de los proyectos
    
    Se agregan animaciones al botón hamburguesa y al menú en la versión móvil.
    
    Se mejoran estilos en la versión desktop.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,15 @@
 const hamburguerBtn = document.querySelector(".header_menu-button")
+const hbgrBtnLine1 = document.querySelector(".bar-1")
+const hbgrBtnLine2 = document.querySelector(".bar-2")
+const hbgrBtnLine3 = document.querySelector(".bar-3")
 const menu = document.querySelector(".header_menu")
 
 
 hamburguerBtn.addEventListener("click", () => {
-    menu.classList.toggle("hidden")
+    // menu.classList.toggle("hidden")
+    menu.classList.toggle("animation")
+    hbgrBtnLine1.classList.toggle("animation")    
+    hbgrBtnLine2.classList.toggle("animation")    
+    hbgrBtnLine3.classList.toggle("animation")
 })
 
